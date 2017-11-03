ce6e9c4a0c46e6fb1a93324c17409ab6a2f5ab54
Mitch <meechllada@gmail.com>
2017-11-03 11:14:42 -0700

    service injected into member-detail component

--- src/app/member-detail/member-detail.component.ts
+++ src/app/member-detail/member-detail.component.ts
@@ -2,16 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 import { Member } from '../member.model';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
+  providers: [MemberService]
 })
 export class MemberDetailComponent implements OnInit {
   memberId: number = null;
 
-  constructor(private route: ActivatedRoute, private location: Location) { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private memberService: MemberService
+  ) { }
 
   ngOnInit() {
     this.route.params.forEach((urlParameters) => {
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
 import { Router } from '@angular/router';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css']
+  styleUrls: ['./members.component.css'],
+  providers: [MemberService]
 })
 export class MembersComponent implements OnInit {
   members: Member [];
 
+  constructor(private router: Router, private memberService: MemberService) { }
+
+  ngOnInit() {
+    this.members = this.memberService.getMembers();
+  }
+
   goToMemberDetail(clickedMember: Member){
     this.router.navigate(['members', clickedMember.id]);
   }
-  constructor(private router: Router) { }
-
-  ngOnInit() {
-  }
-
 }
