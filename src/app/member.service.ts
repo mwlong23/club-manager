8e14d4e7bef70c644b8168b9132a2915072ec96b
Mitch <meechllada@gmail.com>
2017-11-03 11:00:28 -0700

    create member.service

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Member } from './member.model';
+import { MEMBERS } from './mock-members';
 
 @Injectable()
 export class MemberService {
 
   constructor() { }
+  getMembers(){
+    return MEMBERS;
+  }
 
 }
