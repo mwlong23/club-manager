146d858532afe3dd9ab3ad3ac58e05fd9ab102ea
Mitch <meechllada@gmail.com>
2017-11-03 16:27:38 -0700

    add delete method to edit a member

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Member } from './member.model';
-import { MEMBERS } from './mock-members';
+// import { MEMBERS } from './mock-members';
 import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
 
 
@@ -23,5 +23,21 @@ export class MemberService {
 
   getMemberById(memberId: string){
     return this.database.object('members/' + memberId)
+  }
+
+  deleteMember(localMemberToDelete){
+    let memberEntryInFirebase = this.getMemberById(localMemberToDelete.$key);
+    memberEntryInFirebase.remove();
+  }
+
+  updateMember(localUpdatedMember){
+    let memberEntryInFirebase = this.getMemberById(localUpdatedMember.$key);
+    memberEntryInFirebase.update({firstName: localUpdatedMember.firstName,
+                                  lastName: localUpdatedMember.lastName,
+                                  role: localUpdatedMember.role,
+                                  yearsMember: localUpdatedMember.yearsMember,
+                                  bio: localUpdatedMember.bio
+
+    })
   }
 };
