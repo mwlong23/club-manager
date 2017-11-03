bcb33529a9282f20ca780eee19198b931ef88d42
Mitch <meechllada@gmail.com>
2017-11-03 10:58:26 -0700

    create member.service

--- src/app/member-detail/member-detail.component.ts
+++ src/app/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Member } from '../member.model';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  memberId: number = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.memberId = parseInt(urlParameters['id']);
+    });
   }
 
 }
--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,17 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member.model';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
   styleUrls: ['./members.component.css']
 })
 export class MembersComponent implements OnInit {
-  members: Member [] = [
-    new Member("Steve", "Smith", "Co-Founder", 12, "As a lifetime chess player, Steve discovered his passion while creating Seattle Chess Players."),
-    new Member("Vanessa", "Dawson", "Co-Founder", 12, "Vanessa discovered her joy for chess after her first victory against Steve.")
-  ];
+  members: Member [];
 
-  constructor() { }
+  goToMemberDetail(clickedMember: Member){
+    this.router.navigate(['members', clickedMember.id]);
+  }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
